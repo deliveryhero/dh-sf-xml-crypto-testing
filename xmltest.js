f2383e8f4d1d24103e2e92717848e447bb6f2e48
Peter Tempfli <peter.tempfli@deliveryhero.com>
2023-02-17 16:56:19 +0100

    w

--- xmltest.js
+++ xmltest.js
@@ -82,7 +82,11 @@ const signDocument = (payload, privSigKeyPath, elementToSign) => {
 	var sig = new xmlcrypto.SignedXml();
     sig.canonicalizationAlgorithm = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315';
     sig.signatureAlgorithm = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256';
-	sig.addReference("//*[local-name(.)='" + elementToSign + "']");
+	sig.addReference(
+        "//*[local-name(.)='" + elementToSign + "']"
+        ['http://www.w3.org/2000/09/xmldsig#enveloped-signature','http://www.w3.org/TR/2001/REC-xml-c14n-20010315'],
+        []
+        );
 	sig.signingKey = fs.readFileSync(privSigKeyPath)
 	sig.computeSignature(payload)
     // console.log('#### sig ', sig);
