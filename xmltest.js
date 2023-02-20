fd0c4fca4e7d6f53a7e78f9dc3d746ee64db94ab
Peter Tempfli <peter.tempfli@deliveryhero.com>
2023-02-20 10:35:57 +0100

    w

--- xmltest.js
+++ xmltest.js
@@ -17,7 +17,7 @@ const xmlenc = require('xml-encryption');
 const xmldom = require('@xmldom/xmldom');
 
 const ENCRYPTION_PUB_CERT_PATH = '/Users/p.tempfli/mm/citi_dotnet/cert/js1_pub.pem';
-const ENCRYPTION_PUB_CERT = fs.readFileSync(ENCRYPTION_PUB_CERT_PATH);
+// const ENCRYPTION_PUB_CERT = fs.readFileSync(ENCRYPTION_PUB_CERT_PATH);
 const DECRYPTION_PRIV_KEY_PATH = '/Users/p.tempfli/mm/citi_dotnet/cert/js1_priv.pem';
 const DECRYPTION_PRIV_KEY = fs.readFileSync(DECRYPTION_PRIV_KEY_PATH);
 
@@ -29,6 +29,24 @@ const SIGNATURE_PRIVATE_KEY_PATH = '/Users/p.tempfli/mm/citi_dotnet/cert/js_sig.
 const PAYLOAD_SIGNED_ENCRYPTED = '<EncryptedData Type="http://www.w3.org/2001/04/xmlenc#Element" xmlns="http://www.w3.org/2001/04/xmlenc#"><EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#tripledes-cbc" /><KeyInfo xmlns="http://www.w3.org/2000/09/xmldsig#"><EncryptedKey xmlns="http://www.w3.org/2001/04/xmlenc#"><EncryptionMethod Algorithm="http://www.w3.org/2001/04/xmlenc#rsa-1_5" /><CipherData><CipherValue>FFlNq/cWXPz7FrbXVGXYeYGs5wCiBQLEmKFhifkmGIUtqPpxVY9OTdPwkkfZK6fmtZOmG1soes86x3lHDE8a7USOv7CampcDPQ7OjIudPgi0j38zyZ5HT2G+jZOeWnHxQ/R1E1RtNiLnj08K7PQ3RAeowGGpopEZEnlt9AfqjOwXH76kZRlNRjhpYMOSwfg9GhG+/U2BwSb9rocUSFjienKRv/1kSi8bcJcwXVE2myifpqOUiNWG9qPiH9JLOANg4N6k8gustV1Sa65MyTk7gomb84y8c1JLy6scx/0RHs9mrmp5lWW3lmuXfYhCSrlOIGuXx+nMyxYAPttYBnQkrg==</CipherValue></CipherData></EncryptedKey></KeyInfo><CipherData><CipherValue>Qwrgw6dIOt0B0vr9TAI2dQt2GxWklikyL0xFRBDexoy9G38C9ounqbNA/Ho6SeivQRAc2/A4qkj899TlC7fykNMc6L8L3CTjbOPN8rqoYlaWYjYcSMHSfsOfR/8XTCVRByhiWum0ljEoiPu9WA/Cb24xF4oLeBRs97NY8SI5tleZgCGjnW/dd8Bq6IBXtwvE8vFzGCMW1R9nl4M0P8A2WbhMF9aOJ60yLp6GoWcde/haOfFJ1MqBfPa6LJg0YCg+tM0lcS5vLlth/T025YiOwHXc9EPNcQQr0XM75Dkx3xpeJfFbmYgU839EMfNENNOLo38br2spHkqByUnPFbjOJ8MTKH5t17JYw9QTmpvIO/AUsVAZ/N8YXW/CS+r4/HO4rz70ou1Y8y3tkK5nyp4rap4tZeCiQdbYjv8pznldnarusWC7XYhAHe+F6rU6jiNnRrW82BReUnzsLnhmVTFYmtFIgGOVNlKtq6RRFgtZGU+fxljoUxwbfbyS19djHEcaEfhZg8e/MgxRM42OKcjjuIvVVLP2UiYocmSLfvwPK7q+s4anQOhbQkNsjvIW9IEpSpuL7wMJ+awFVEGdoMS+1QWWLM1uBBVE77I9qBJImQfcusnxj7suU+HjXwqez9fSFfk8DY9kVMwVSKRh742Kj4hPbVF9iUjJVnxX2ouiPwNbCHz7++lKKjfpCje6L34KWzkK4b9970+gfYsvJ25ktZQLaRzVxuFrBQwGzI74/YvmsYjBcIO/PoBDdJTVnARp0oYnc4p86gu4c5x8uxu2tZWHsQY0IVn5Nu4CAGQNEEzIT1rseVU0IA0B9N1OQdhOxKKlOkp9cMqXfalqzOXjbvCUaYDDP2bIsI8hkoAjJvw/z1WTNGq6Qez2frQtqkL9E45onzWj+hG8Rs+QYTUw7z9SeGQsMsVPCSeU5hhs+7DD9BG4tHT975KaAz4vTH3AJ5ukxkLj4Ybt5rCc3RZicUDfibdNRz3w1Q2HXC0fty2BVLkn/quwIfzdJ0A0H4YV022uNV5YdLHpIriVTZFgKLJXhNIqyMdQli8SNvweK/NhqDM0Ju0lS+VhPWdio3miiLGTRzkNxEdB3/ve8C9fiPTzMIjczoz47xmzagNTR7S5vrIg0meDiNHuWkLuOP0ZMyZxWBmAqGPtCKMnQ3WJ0soo8ZMnzC69xfUEWxpxrvxjZLoxzL1ns2bu8f5KE+Cfwgx4Da13K8NFfzQrsdG6tf1TGCdihGIcONKG7aqb4eNnx5FFCDFP0of2924S0NZBuXlN4SI79t2OoLM1uPa33VJMXMAM0doA7ElXHxV4zcB4khcjbNTYWy+EWWS0yvgRKYwwZNhV02buETt5sFzUe0tXFsWdbZkdgtFDgFrHQKirF0FdyKNA+3/W57TXcqRSCFj2+hcGCxxHY7RufOwlIueaAPmjQv9wY9/YLgydTC/8As/yq99jgCqA385Z2O+aaI+KUOxLnCS7WvmVMVgcKTOxiaQULK4n0vNjOzPG9/5KRiFCBvWdqQzPpSSnlry9qKoC4KAlqvLPlPJdLOEy04KrrhMGrz+ck/dwFskWLkXXWC4obs7Aa6al8LQQLIg6Mg+UEkoPsDiGQJoEvzqBNgKM7/lsfWaZA7eyOUl6jUb3Fl21sYuiH1aJ1qdJ2Z11p0Jr8yMjjFhalgETWDM/skQ2C+okycEvvPym6v11NPLcspFtMJV5EaIzOPzm3KBxIoO65wtLQttxKzDebQ0FxjUZ3Emx0LnjJmXhdOFcH2iB8jWFHBiqKAU2Fz5xzo155HM1bfxZJ5G/dzohsibGMbgNAnnNuzGQ86qR1HCCTMe0Dox3ohJMJDnOWeq1U/VYrsOFzS1p8wa//nU+7+Pnmd9tw/4tTGboZaiVHk8CuwUat7zEPmMHpYcljMQ8oJycK6b9P8vYm5SaV/9c06lfLXDphkldUv2o7onhxvx+poPCkuvPzAeVTIEY6M6VhDtgaHpiA4K7iKaTTLPFcMWLKe4sKH5Hwreo34Ajc6nKZd0nSHD4Az5H/4J5PF0iT2ueSHUJbS6ePaA1XrOgzwo57ycLFKqBCfK9N657hIbaGX+fxgt4hOAG4FXr0avy7ZWeMxSWWAbr9zSpveXMPGV5XmcwRBH5K8TLMNebnrq46OcLZ2uR82mWdellEs70mfVNxBv6H49BzgsmTX5cyjKthSV4dDvAO0nTQJOTaAkTsfEjNpI5yb6n1BD4iG+gcMU9LcHdfRf8HarFEUDB3IvBzBUBNIEpOO/b3hb0yGJ04F82shfr4tGms4TowmNHGyB9u1QFTrNvDKuzAUOnTFxmvwxVYcnjqK2kLdHLqyQmL0nEEuKpLpIDejysm/QAVyJJUbCv9eQ451MeFRkDyyK3T4EXNS81CKMKpVA4Obidod8je3AMKNbn8ZYTaif5cMOPAIWX61v+sgBqX6WZYGgmORDDvrd+c2186tjBx08h3Yjydie55G3VWReI5L88EB+DS+bUwa1B8wIflzDLybSFPu0+t7YZaHI8gHFg7AxaotOzEG9z9ML1pb1vKUCqet76CAlKkL3lhjbt8sPbwf6VDwo3Y03ezB6LHao461t6EtpO3m8V4eisIrGDNxGynRwd8A3obeflqP/9ouTYd8a1Ka/pfDaOOSoQn7Kbx2HKC68n2AVJjmNeDniU56DrYoi1P7AgC3twVY40QW1Zg15SDCwd91hm/I8817svGuP1gsdbg77tKjHgzHDC4BdpTebyqItIrBmaLUxntjuHoCWeN3mZUqjdA5Wsk3s+uQknrAG9Q2vcYXr/OHhPHVlVTogIMgteuCO533CpMUBsQdANmLytqdJapYdikiFB5VofjG4zYLnOsdcIKtTRJ47ApiwYDeFOEDezyOr6OLs7x4EWUUFEoRsGmEO+yWlxGkjqiEAAbIPNSotWfgwB8umEslZwKoWj7sVcP6LIGRscwETVf8AaVdCeuG8v40pQtkzgOaknD7cQuJf8aj49OphnjTPH8AEV+RHNvFX7gDBhR47zTGzpDVNH0+HXkiHJudzaWXa9s+sDOFFGkpuGOxQ/H1wnkaUCY4cR/74ax9CJd8n47HyT+FfaCgjvPEXsiDwsmiXmMxsBe1LZtTUk+11Ydv0J</CipherValue></CipherData></EncryptedData>';
 // const PAYLOAD2 = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><html><body><h1>Hellox Peter</h1><h2>jahhha</h2></body></html>';
 const PAYLOAD3 = '<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<oAuthToken\nxmlns=\"http://com.citi.citiconnect/services/types/oauthtoken/v1\" Id="myId">\n<grantType>client_credentials</grantType>\n<scope>/authenticationservices/v1</scope></oAuthToken>';
+
+const encrpytXml = async(payload, publicKeyPath) => {
+    var options = {
+        rsa_pub: fs.readFileSync(publicKeyPath),
+        pem: fs.readFileSync(publicKeyPath),
+        encryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#aes256-cbc',
+        keyEncryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p',
+        disallowEncryptionWithInsecureAlgorithm: true,
+        warnInsecureAlgorithm: true
+      };
+      
+      return new Promise((resolve, reject) => {
+        xmlenc.encrypt(payload, options, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+        });
+      });
+}
 
 const decryptXml = async (payload, privateKey) => {
     return new Promise((resolve, reject) => {
@@ -73,12 +91,6 @@ const verifySignature = (myDoc, publicSigKeyPath) => {
 const signDocument = (payload, privSigKeyPath, elementToSign) => {
 	var SignedXml = require('xml-crypto').SignedXml	  
 
-	var xml = "<library>" +
-	            "<book>" +
-	              "<name>Harry Potter</name>" +
-	            "</book>" +
-	          "</library>"
-
 	var sig = new xmlcrypto.SignedXml();
     sig.canonicalizationAlgorithm = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315';
     sig.signatureAlgorithm = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256';
@@ -101,9 +113,16 @@ const main = async() => {
     // verifySignature(myDoc);
 
     let signedDoc = signDocument(PAYLOAD3, SIGNATURE_PRIVATE_KEY_PATH, 'oAuthToken'); //oAuthToken');
-    // signedDoc = signedDoc.replace(' Id="_0"', '');
     console.log('### signedDoc', signedDoc);
-    verifySignature(signedDoc, SIGNATURE_PUBLIC_KEY_PATH);
+    console.log('.................................................\n\n');
+    const encryptedDoc = await encrpytXml(signedDoc, ENCRYPTION_PUB_CERT_PATH);
+    console.log('### encryptedDoc', encryptedDoc);
+    console.log('.................................................\n\n');
+    const decryptedDoc = await decryptXml(encryptedDoc, DECRYPTION_PRIV_KEY);
+    console.log('### decryptedDoc', decryptedDoc);
+    verifySignature(decryptedDoc, SIGNATURE_PUBLIC_KEY_PATH); 
+
+
 }
 
 main();
